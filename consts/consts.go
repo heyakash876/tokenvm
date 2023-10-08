fdd9f18945387d419bcec965f702a2a003f483f9
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2023-10-08 17:02:58 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Akash"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "AKcoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "AKA"
 )
 
 var ID ids.ID
